77f70bb1d670382261a56a2d5f290cc3091531f1
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2022-10-20 18:12:55 +0900

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('findfilefromselection.find', () => {
+		const editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "findfilefromselection" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('findfilefromselection.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VSCode!');
-
-		if (!vscode.window.activeTextEditor) {
-			console.error('editor is not found');
+		if (!editor) {
+			vscode.commands.executeCommand('workbench.action.quickOpen');
 			return;
 		}
-		console.log(vscode.window.activeTextEditor.selection.start, vscode.window.activeTextEditor.selection.end);
-		console.log(vscode.window.activeTextEditor.selections);
+
+		const { start, end } = editor.selection;
+		const selectedText = editor.document.getText(new vscode.Range(start, end));
+
+		// Open quickOpen dialog. The placeholder is selected text.
+		vscode.commands.executeCommand('workbench.action.quickOpen', selectedText);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
